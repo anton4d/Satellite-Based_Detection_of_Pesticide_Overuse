aa119cf0dbc95039f9f41a90733554485187abe5
Anton4d <Antonlukas25@gmail.com>
2025-02-17 15:55:05 +0100

    Created CatalogApiHandler, found error with gis Coordinates not compatible with api
    - Created CatalogApiHandler file
    - Created GetAccess token method inside the file
    - Created GetPictureDates method indide file
    	- the method is stil not fuctioning since failure point between gis coordinates using EPSG 25832 not compatible with api expecting EPSG:4326
    - next step convert EPSG 25832 to EPSG 4326

--- Backend/Main.py
+++ Backend/Main.py
@@ -3,6 +3,7 @@ import dotenv
 import os
 from Gis.geojsonToDB import GeoJsonToDB
 from Database.SQLHandler import SQLHandler
+from Api.CatalogApiHandler import CatalogApiHandler
 
 def setup_logging(log_file="app.log"):
     logging.basicConfig(
@@ -26,7 +27,30 @@ def main():
     geojson_path = '../Shapefiles/Marker_2019.geojson'
 
     geojsonToDB = GeoJsonToDB(geojson_path, db_handler)
-    geojsonToDB.process_geojson()
+    #geojsonToDB.process_geojson()
+
+    polygon_wkt = ("POLYGON ((659478.154481784 6149596.98423771, "
+               "659493.30449996 6149614.07274354, "
+               "659493.436744312 6149613.98295175, "
+               "659493.406906907 6149613.94859353, "
+               "659543.552771315 6149579.954976, "
+               "659529.096124877 6149562.96145264, "
+               "659514.742468898 6149572.32873378, "
+               "659506.51451136 6149577.96234791, "
+               "659489.115440714 6149589.44857846, "
+               "659478.154481784 6149596.98423771))")
+    
+    Catalog_ApiHandler = CatalogApiHandler(
+        ClientId=os.getenv("ApiClienId"),
+        ClientSecret=os.getenv("ApiClienSecret"),
+        FromDate="2024-12-01T00:00:00Z",
+        ToDate="2025-01-01T23:59:59Z",
+        ApiToken=os.getenv("APIToken")
+        )
+
+    Catalog_ApiHandler.GetPictureDates(PolygonWkt=polygon_wkt)
+
+
 
 
 
