6e6509acbec032ae41c3ee82425b5bc561558fa8
OKThomsen <ostho20@student.sdu.dk>
2025-02-18 12:28:07 +0100

    Two separate scripts that filters GiS shapefile data to geoJSON files.
    ShapefileDatasetToGeoJSON.py filters relevant fields (potatoes) and their features into a singulair geoJSON file.
    
    geoJSONFeatureSplitter.py takes the geoJSON file created by ShapefileDatasetToGeoJSON.py and splits the geoJSON file into separate geoJSON files, that only contain a single feature(field). This could prove useful for testing purposes.

---
+++ Backend/Gis/ShapefileDatasetToGeoJSON.py
@@ -0,0 +1,38 @@
+import geopandas as gpd
+import os
+
+# Function to convert shapefile to GeoJSON
+def shapefile_to_geojson(shapefile_path, geojson_output_path):
+    try:
+        print(f"Reading shapefile: {shapefile_path}")
+        gdf = gpd.read_file(shapefile_path)
+        
+        # Checking the number of features to understand the data size
+        print(f"Number of features in the shapefile: {len(gdf)}")
+        
+        filter_values = [
+            "Kartofler, lægge- (certificerede)",
+            "Kartofler, lægge- (egen opformering)",
+            "Kartofler, spise-"
+        ]
+
+        # Ensure matching is case-insensitive and strip any extra spaces
+        gdf['Afgroede'] = gdf['Afgroede'].str.strip().str.lower()
+        filter_values = [value.lower() for value in filter_values]
+        
+
+        filtered_gdf = gdf[gdf['Afgroede'].isin(filter_values)]
+
+        # Convert the GeoDataFrame to GeoJSON format
+        print(f"Writing GeoJSON to: {geojson_output_path}")
+        filtered_gdf.to_file(geojson_output_path, driver="GeoJSON")
+        
+        print(f"Conversion successful! The GeoJSON file has been saved at: {geojson_output_path}")
+    except Exception as e:
+        print(f"Error during conversion: {e}")
+
+
+if __name__ == "__main__":
+    shapefile_path = 'localpath'
+    geojson_output_path = '../../Shapefiles/Marker_2020.geojson'
+    shapefile_to_geojson(shapefile_path, geojson_output_path)
