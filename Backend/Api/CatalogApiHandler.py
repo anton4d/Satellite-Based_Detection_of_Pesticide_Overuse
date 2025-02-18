09f0d155d443e1e13878b7f1fa396ed602d9a22a
Anton4d <Antonlukas25@gmail.com>
2025-02-18 11:42:22 +0100

    The CatalogApiHandler now returns a list of dates so it is done
    issue # 8 done
    - In Main.py changed The mock up data so it works with the api
    - In Main.py added FeildId to the mock up data
    - Added a loop so if token is incorrect get token and call method again if it gets a new token
    - Added a for loop trough the dict of Features from the response To extract the datetime
    - extracted only the date from Datetime
    - makes a list with all the unique dates and returns it to be used in the procces api
    
    - made more logging.info into .debug to make debugging easier
    - made raise of exceptions to erroes to stop the program if an error is found

--- Backend/Api/CatalogApiHandler.py
+++ Backend/Api/CatalogApiHandler.py
@@ -18,32 +18,50 @@ class CatalogApiHandler:
             self.ApiToken = ApiToken
 
     def GetToken(self):
-        logging.info("Api Call to get An Api Token")
+        """Gets an Access Token from the API based on the client credentials"""
+
+        logging.info("Attempting to get an API Token...")
+        
         dotenvFile = dotenv.find_dotenv()
-        AuthServerUrl="https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
-        TokenRequestPaylod = {"grant_type": "client_credentials"}
+        AuthServerUrl = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+        TokenRequestPayload = {"grant_type": "client_credentials"}
 
-        tokenResponse = requests.post(AuthServerUrl,
-        data=TokenRequestPaylod, verify=False, allow_redirects=False,
-        auth=(self.ClientId,self.ClientSecret))
+        tokenResponse = requests.post(
+            AuthServerUrl,
+            data=TokenRequestPayload,
+            verify=False,
+            allow_redirects=False,
+            auth=(self.ClientId, self.ClientSecret)
+        )
+
         if tokenResponse.status_code != 200:
-            logging.error(f"Failed to obtain token from the OAuth 2.0 server Response status code: {tokenResponse.status_code}")
-        tokendump = json.dumps(tokenResponse.json(), indent=4)
-        token = tokenResponse.json()
-        logging.info(f"Successfuly obtained a new token with values: {tokendump}")
-        self.ApiToken = token["access_token"]
-        dotenv.set_key(dotenvFile, "APIToken", token["access_token"])
+            logging.error(f"Failed to obtain token. Status code: {tokenResponse.status_code}")
+            logging.error(f"Response: {tokenResponse.text}")
+            raise Exception("Authentication failed. Check your credentials.")
 
-    
-    def GetPictureDates(self,PolygonWkt):
-        """Gets a list of dates between the from and To date Where the Satalite took a picture based on a polygon"""
-        logging.info("Calling the Catalog for catalog data on Feild with id: 1")
+        token = tokenResponse.json()
+        self.ApiToken = token["access_token"]
+        dotenv.set_key(dotenvFile, "APIToken", self.ApiToken)
+
+        logging.info("Successfully obtained a new token.")
+
+
+    def ConvertWktToNestedCords(self, PolygonWkt):
+        """Converts a Wkt To the the Nested structure that the api takes"""
         polygon = wkt.loads(PolygonWkt)
 
-        exterior_coords = list(polygon.exterior.coords)
+        exteriorCoords = list(polygon.exterior.coords)
 
-        nested_coords = [[list(coord) for coord in exterior_coords]]
-        print(nested_coords)
+        NestedCoords = [[list(coord) for coord in exteriorCoords]]
+        return NestedCoords
+
+    
+    def GetPictureDates(self,PolygonWkt, FeildId):
+        """Gets a list of dates between the from and To date Where the Satalite took a picture based on a polygon"""
+
+        logging.info(f"Calling the Catalog api for catalog data on Feild with id: {FeildId}...")
+        
+        polygonWithNestedCoords = self.ConvertWktToNestedCords(PolygonWkt)
 
         url = "https://sh.dataspace.copernicus.eu/api/v1/catalog/1.0.0/search"
         headers = {
@@ -57,19 +75,34 @@ class CatalogApiHandler:
             "datetime": f"{self.FromDate}/{self.ToDate}",
             "intersects": {
                 "type": "Polygon",
-                "coordinates": nested_coords
+                "coordinates": polygonWithNestedCoords
             },
             "limit": 100
         }
         response = requests.post(url, headers=headers, json=data)
         responseJson = response.json()
         if response.status_code == 200:
-            json_object = json.dumps(responseJson, indent=4)
+            Features = responseJson.get("features", [])
+            logging.info(f"Found {len(Features)} number of features")
+            uniqueDates = set()
+            for Feature in Features:
+                properties = Feature.get("properties", {})
+                datetime = properties.get("datetime", "No datetime provided")
+                logging.debug(f"Found Feature with datetime: {datetime}")
+                dateOnly = datetime.split("T")[0]
+
+                if dateOnly not in uniqueDates:
+                    uniqueDates.add(dateOnly)
+                    logging.debug(f"Added new date: {dateOnly}")
             with open("ResponseCatalog.txt", "w") as f:
-                f.write(json_object)
-        if response.status_code == 401:
-            logging.error("AccesCode has expired or is wrong")
+                json.dump(responseJson, f, indent=4)
+            return uniqueDates
+        elif response.status_code == 401:
+            logging.error("Access code has expired or is incorrect.")
             self.GetToken()
+            Data = self.GetPictureDates(PolygonWkt=PolygonWkt, FeildId=FeildId)
+            return Data
         else:
             description = responseJson.get("description", "No description provided")
-            logging.error(f"Request failed with status code: {response.status_code} and description: {description}")
+            logging.error(f"Request failed (Status: {response.status_code}) - {description}")
+            raise Exception(f"API request failed with status {response.status_code}: {description}")
--- Backend/Main.py
+++ Backend/Main.py
@@ -27,28 +27,31 @@ def main():
     geojson_path = '../Shapefiles/Marker_2019.geojson'
 
     geojsonToDB = GeoJsonToDB(geojson_path, db_handler)
-    #geojsonToDB.process_geojson()
+    geojsonToDB.process_geojson()
 
-    polygon_wkt = ("POLYGON ((659478.154481784 6149596.98423771, "
-               "659493.30449996 6149614.07274354, "
-               "659493.436744312 6149613.98295175, "
-               "659493.406906907 6149613.94859353, "
-               "659543.552771315 6149579.954976, "
-               "659529.096124877 6149562.96145264, "
-               "659514.742468898 6149572.32873378, "
-               "659506.51451136 6149577.96234791, "
-               "659489.115440714 6149589.44857846, "
-               "659478.154481784 6149596.98423771))")
+    #Muck up Polygon_Wkt data and Feild Id Create method that gets data from the database
+
+    PolygonWkt = "POLYGON ((" \
+        "9.955577 55.729285, 9.956843 55.729188, 9.958131 55.729418, 9.959075 55.729599, " \
+        "9.96017 55.729805, 9.960942 55.729962, 9.961973 55.730119, 9.963046 55.730336, " \
+        "9.964956 55.730566, 9.965814 55.730675, 9.966372 55.730469, 9.967102 55.73024, " \
+        "9.967488 55.730179, 9.96781 55.73001, 9.968325 55.729611, 9.968067 55.729358, " \
+        "9.966479 55.728886, 9.9659 55.7285, 9.963196 55.727352, 9.962702 55.726977, " \
+        "9.961629 55.726566, 9.960985 55.726288, 9.959805 55.726325, 9.95841 55.726433, " \
+        "9.957423 55.72647, 9.956672 55.726554, 9.954891 55.726603, 9.954504 55.726603, " \
+        "9.954461 55.727521, 9.954504 55.72798, 9.95474 55.728427, 9.954976 55.728512, " \
+        "9.95517 55.728717, 9.955384 55.728935, 9.955577 55.729285))"
+    FeildId = 1
     
     Catalog_ApiHandler = CatalogApiHandler(
         ClientId=os.getenv("ApiClienId"),
         ClientSecret=os.getenv("ApiClienSecret"),
-        FromDate="2024-12-01T00:00:00Z",
-        ToDate="2025-01-01T23:59:59Z",
+        FromDate="2024-10-01T00:00:00Z",
+        ToDate="2025-02-01T23:59:59Z",
         ApiToken=os.getenv("APIToken")
         )
 
-    Catalog_ApiHandler.GetPictureDates(PolygonWkt=polygon_wkt)
+    CatalogData = Catalog_ApiHandler.GetPictureDates(PolygonWkt=PolygonWkt,FeildId=FeildId)
 
 
 
