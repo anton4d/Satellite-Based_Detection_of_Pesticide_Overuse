46dd056fd290b3649d1579d23296eaf1a68d1544
OKThomsen <ostho20@student.sdu.dk>
2025-02-24 11:19:47 +0100

    Fixed Timing Bug
    Added a sleep timer to ensure that the CatalogApiHandler finishes, before the ProcessApiHandler starts, as this was causing issues.

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,6 +1,7 @@
 import logging
 import dotenv
 import os
+import time
 from Gis.geojsonToDB import GeoJsonToDB
 from Gis.WktHandler import ConvertWktToNestedCords
 from Database.SQLHandler import SQLHandler
@@ -67,13 +68,10 @@ def main():
     CatalogData = Catalog_ApiHandler.GetPictureDates(Polygon=polygon,FeildId=first_field_id)
     logging.info(f"Catalog Api found: {len(CatalogData)} number of dates")
     dotenv.load_dotenv(dotenvFile)
+    time.sleep(3) # Sleep timer to ensure that the CatalogApiHandler finishes before ProcessApiHandler starts
     for date in CatalogData:
-        Process_ApiHandler.processDateIntoImages(date,polygon,first_field_id)
-    
+        Process_ApiHandler.processDateIntoImages(date,polygon,first_field_id)   
     logging.info("Stopping the Download application...")
-
-
-
 
 
 if __name__ == "__main__":
