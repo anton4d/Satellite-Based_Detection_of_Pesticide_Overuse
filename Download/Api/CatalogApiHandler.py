63d945b32c15646dfd08f3fd375bfd57de57a744
Anton4d <Antonlukas25@gmail.com>
2025-02-25 15:14:28 +0100

    used a context manager to make sure that the response closes

--- Download/Api/CatalogApiHandler.py
+++ Download/Api/CatalogApiHandler.py
@@ -44,38 +44,38 @@ class CatalogApiHandler:
         }
         if next != 0:
             data["next"] = next
-        response = requests.post(url, headers=headers, json=data)
-        responseJson = response.json()
-        if response.status_code == 200:
-            Features = responseJson.get("features", [])
-            logging.info(f"Found {len(Features)} number of features")
-            uniqueDates = set()
-            for Feature in Features:
-                properties = Feature.get("properties", {})
-                datetime = properties.get("datetime", "No datetime provided")
-                logging.debug(f"Found Feature with datetime: {datetime}")
-                dateOnly = datetime.split("T")[0]
+        with requests.post(url, headers=headers, json=data) as response:
+            responseJson = response.json()
+            if response.status_code == 200:
+                Features = responseJson.get("features", [])
+                logging.info(f"Found {len(Features)} number of features")
+                uniqueDates = set()
+                for Feature in Features:
+                    properties = Feature.get("properties", {})
+                    datetime = properties.get("datetime", "No datetime provided")
+                    logging.debug(f"Found Feature with datetime: {datetime}")
+                    dateOnly = datetime.split("T")[0]
 
-                if dateOnly not in uniqueDates:
-                    uniqueDates.add(dateOnly)
-                    logging.debug(f"Added new date: {dateOnly}")
-            with open("ResponseCatalog.txt", "w") as f:
-                json.dump(responseJson, f, indent=4)
-            context = responseJson.get("context", [])
-            logging.debug(context)
-            next = context.get("next", "No next")
-            logging.debug(next)
-            if next != "No next":
-                logging.debug(f"list has:{len(uniqueDates)} dates in it before next")
-                uniqueDates.update(self.GetPictureDates(Polygon, FeildId, next)) 
-                logging.debug(f"list has:{len(uniqueDates)} dates in it after next")
-            return uniqueDates
-        elif response.status_code == 401:
-            logging.error("Access code has expired or is incorrect.")
-            self.ApiToken = self.TokenApiHandler.GetToken()
-            Data = self.GetPictureDates(Polygon=Polygon, FeildId=FeildId)
-            return Data
-        else:
-            description = responseJson.get("description", "No description provided")
-            logging.error(f"Request failed (Status: {response.status_code}) - {description}")
-            raise Exception(f"API request failed with status {response.status_code}: {description}")
+                    if dateOnly not in uniqueDates:
+                        uniqueDates.add(dateOnly)
+                        logging.debug(f"Added new date: {dateOnly}")
+                with open("ResponseCatalog.txt", "w") as f:
+                    json.dump(responseJson, f, indent=4)
+                context = responseJson.get("context", [])
+                logging.debug(context)
+                next = context.get("next", "No next")
+                logging.debug(next)
+                if next != "No next":
+                    logging.debug(f"list has:{len(uniqueDates)} dates in it before next")
+                    uniqueDates.update(self.GetPictureDates(Polygon, FeildId, next)) 
+                    logging.debug(f"list has:{len(uniqueDates)} dates in it after next")
+                return uniqueDates
+            elif response.status_code == 401:
+                logging.error("Access code has expired or is incorrect.")
+                self.ApiToken = self.TokenApiHandler.GetToken()
+                Data = self.GetPictureDates(Polygon=Polygon, FeildId=FeildId)
+                return Data
+            else:
+                description = responseJson.get("description", "No description provided")
+                logging.error(f"Request failed (Status: {response.status_code}) - {description}")
+                raise Exception(f"API request failed with status {response.status_code}: {description}")
