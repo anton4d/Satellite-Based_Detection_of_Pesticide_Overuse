72cd2e41b40fa590d5fbf8523dc41318e09c9344
Anton4d <Antonlukas25@gmail.com>
2025-03-03 11:25:29 +0100

    starting playing with the rastertio package
    playing araund with the package to get a better understanding of the tiff data

---
+++ Process/Database/Database.md
@@ -0,0 +1,37 @@
+# Database 
+
+## structure 
+
+### fieldTable
+
+- Name: fieldID
+    - Data: ID
+- Name: crops
+    - Data: VARCHAR
+- Name: MarkNr
+    - Data: VARCHAR
+- Name: CVR
+    - Data: VARCHAR
+- Name: averageRed
+    - Data: VARCHAR
+- Name: AverageNirRed
+    - Data: VARCHAR
+- Name: AverageNDVI
+    - Data: VARCHAR
+- Name: polygon
+    - Data: POLYGON
+- Name: TimeStamp
+    - Data: TimeStamp
+- Name: UPDATEStamp
+    - Data: TimeStamp
+### TileTable
+- Name: FieldID
+    - Data: ID
+- Name: tileId
+    - Data: ID
+- Name: NirRed
+    - Data: VARCHAR
+- Name: Red
+    - Data: VARCHAR
+- Name: NDVI
+    - Name: VARCHAR
---
+++ Process/Main.py
@@ -0,0 +1,36 @@
+import logging, os, dotenv
+from ToDB.Todb import ToDb 
+from Database.SQLHandler import SQLHandler
+
+def setup_logging(log_file="Process.log"):
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,  
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main():
+    logging.info("Starting the Process aplication...")
+    dotenvFile = dotenv.find_dotenv()
+    dotenv.load_dotenv(dotenvFile)
+
+    db_handler = SQLHandler(
+        host=os.getenv("SQLHOST"),
+        user=os.getenv("DBUSER"),
+        password=os.getenv("DBPASSWORD"),
+        database=os.getenv("DBDB")
+    )
+    To_Db = ToDb(
+        db_handler
+    )
+    imagePath="../Download/Pictures/FieldId1/Test.tiff"
+    To_Db.TiffToDatabase(imagePath)
+    
+    logging.info("Stopping the Process aplication")
+
+
+if __name__ == "__main__":
+    setup_logging() 
+    main()
